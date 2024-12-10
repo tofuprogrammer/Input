a67788fa41121a8dc82a2ad7cc731cb80af7e341
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2024-12-10 10:25:18 +0000

    Completed and versioned task 2.

---
+++ Task 2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int currentYear = DateTime.Now.Year;
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+        Console.Write("Enter the year you were born: ");
+        int userBirthYear = int.Parse(Console.ReadLine());
+        Console.Write("Hello, ");
+        Console.Write(userName);
+        Console.Write("! You are ");
+        int userAge = currentYear - userBirthYear;
+        Console.Write(userAge);
+        Console.WriteLine(" years old.");
+    }
+}
