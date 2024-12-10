991cd6fbc6c3dddb71c6a3914c3d9cc23c976027
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2024-12-10 10:46:47 +0000

    Completed and versioned task 4.

---
+++ Task 4/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        double firstUserNumber = double.Parse(Console.ReadLine());
+        Console.Write("Enter another number: ");
+        double secondUserNumber = double.Parse(Console.ReadLine());
+        Console.Write(firstUserNumber);
+        Console.Write(" + ");
+        Console.Write(secondUserNumber);
+        Console.Write(" = ");
+        double addition = firstUserNumber + secondUserNumber;
+        Console.WriteLine(addition);
+        Console.Write(firstUserNumber);
+        Console.Write(" * ");
+        Console.Write(secondUserNumber);
+        Console.Write(" = ");
+        double multiplication = firstUserNumber * secondUserNumber;
+        Console.WriteLine(multiplication);
+        Console.Write(firstUserNumber);
+        Console.Write(" - ");
+        Console.Write(secondUserNumber);
+        Console.Write(" = ");
+        double subtraction = secondUserNumber - firstUserNumber;
+        Console.WriteLine(subtraction);
+        if (secondUserNumber == 0)
+        {
+            Console.WriteLine("Error. Cannot divide by zero.");
+        }
+        else
+        {
+            Console.Write(firstUserNumber);
+            Console.Write(" / ");
+            Console.Write(secondUserNumber);
+            Console.Write(" = ");
+            double division = firstUserNumber / secondUserNumber;
+            Console.WriteLine(division);
+        }
+    }
+}
