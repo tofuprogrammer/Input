8cf381e8c8612ab85319fd102f27f719f4bd0162
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2024-12-10 10:38:21 +0000

    Completed and versioned task 3.

---
+++ Task 3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the temperature in Celsius: ");
+        double celsius = double.Parse(Console.ReadLine());
+        double fahrenheit = (celsius * 1.8) + 32;
+        double kelvin = celsius + 273.15;
+        Console.Write(celsius);
+        Console.Write(" Celsius is ");
+        Console.Write(kelvin);
+        Console.WriteLine(" Kelvin.");
+        Console.Write(celsius);
+        Console.Write(" Celsius is ");
+        Console.Write(fahrenheit);
+        Console.WriteLine(" Fahrenheit.");
+    }
+}
