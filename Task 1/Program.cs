25daf0bfcc5ca81eac1654e0ee91b9ed550b7769
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2024-12-10 10:15:45 +0000

    Completed task 1.

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Enter a number: ");
+        int firstUserNumber = int.Parse(Console.ReadLine());
+        Console.Write("Enter another number: ");
+        int secondUserNumber = int.Parse(Console.ReadLine());
+        Console.Write("The sum of your numbers is: ");
+        int sum = firstUserNumber + secondUserNumber;
+        Console.WriteLine(sum);
     }
 }
